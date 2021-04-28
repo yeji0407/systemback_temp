dbd0e79103cd10edb353d5dc89d1be504e6ef3d9
yeji0407 <yejisoft@gmail.com>
2021-04-28 16:47:06 +0900

    test, token revealed

--- push.sh
+++ push.sh
@@ -13,7 +13,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add new https://${GH_TOKEN}@github.com/yeji0407/systemback_temp.git > /dev/null 2>&1
+  git remote add new https://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@github.com/yeji0407/systemback_temp.git > /dev/null 2>&1
   git push --quiet --set-upstream new deploy
 }
 
