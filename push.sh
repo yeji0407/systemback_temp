dc0505350b16c1ea71ec22b00f5368f27e13992f
yeji0407 <yejisoft@gmail.com>
2021-04-29 14:57:23 +0900

    add .git/credentials, remote -v in push.sh

--- push.sh
+++ push.sh
@@ -3,6 +3,9 @@
 setup_git() {
   git config --global user.email "yejisoft@gmail.com"
   git config --global user.name "Hong Yeji"
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+  cat .git/credentials
 }
 
 commit_website_files() {
@@ -13,7 +16,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add new https://${GH_TOKEN}@github.com/yeji0407/test.git > /dev/null 2>&1
+  git remote -v
+  git remote add new https://${GH_TOKEN}:@github.com/yeji0407/test.git > /dev/null 2>&1
   git push --quiet --set-upstream new deploy
 }
 
