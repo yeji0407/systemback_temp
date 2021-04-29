c584f2043e86cee4253143d3a4cae82d9d5f3e3b
yeji0407 <yejisoft@gmail.com>
2021-04-29 15:15:56 +0900

    travis encrypt -r repo

--- push.sh
+++ push.sh
@@ -3,9 +3,6 @@
 setup_git() {
   git config --global user.email "yejisoft@gmail.com"
   git config --global user.name "Hong Yeji"
-  git config credential.helper "store --file=.git/credentials"
-  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-  cat .git/credentials
 }
 
 commit_website_files() {
@@ -16,9 +13,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote -v
-  # git remote add new https://${GH_TOKEN}:@github.com/yeji0407/test.git > /dev/null 2>&1
-  git push origin deploy
+  git remote add new https://${GH_TOKEN}@github.com/yeji0407/systemback_temp.git > /dev/null 2>&1
+  git push new deploy
 }
 
 setup_git
